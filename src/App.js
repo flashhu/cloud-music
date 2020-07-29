d0f89f48a37ebfa2a4ed92e9bc185644c5635b66
flashhu <flashhu@163.com>
2020-07-29 21:13:40 +0800

    feat：推荐页轮播图及列表

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import routes from './routes';
 import { HashRouter } from 'react-router-dom';
 import { GlobalStyle } from './style';
 import { IconStyle } from './assets/iconfont/iconfont';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      { renderRoutes(routes) }
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        { renderRoutes(routes) }
+      </HashRouter>
+    </Provider>
   );
 }
 
